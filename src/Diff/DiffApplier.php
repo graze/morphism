24655b914d19e5734d7837aa20e8becce4937cc8
Will Pillar <will.pillar@graze.com>
2015-05-12 15:21:02 +0100

    Tidying DiffApplier

--- src/Diff/DiffApplier.php
+++ src/Diff/DiffApplier.php
@@ -3,6 +3,7 @@
 namespace Graze\Morphism\Diff;
 
 use Doctrine\DBAL\Connection;
+use Graze\Morphism\Console\Output\OutputHelper;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -10,18 +11,36 @@ use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class DiffApplier
 {
+    /**
+     * @var InputInterface
+     */
     private $input;
+
+    /**
+     * @var OutputInterface
+     */
     private $output;
+
+    /**
+     * @var OutputHelper
+     */
+    private $outputHelper;
 
     /**
      * @var QuestionHelper
      */
     private $question;
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param QuestionHelper $question
+     */
     public function __construct(InputInterface $input, OutputInterface $output, QuestionHelper $question)
     {
         $this->input = $input;
         $this->output = $output;
+        $this->outputHelper = new OutputHelper($output);
         $this->question = $question;
     }
 
@@ -58,22 +77,20 @@ class DiffApplier
 
         $defaultResponse = 'yes';
 
-        foreach($diff->getQueries() as $query) {
+        foreach ($diff->getQueries() as $query) {
             $response = $defaultResponse;
             $apply = false;
 
             if ($confirm) {
-                $this->output->writeln('');
-                $this->output->writeln('<info>' . $query . '</info>');
-                $this->output->writeln('');
+                $this->outputHelper->sql($query);
 
-                $question = new ChoiceQuestion(
+                $choice = new ChoiceQuestion(
                     '-- Apply this change?',
                     ['y' => 'yes', 'n' => 'no', 'a' => 'all', 'q' => 'quit']
                 );
-                $question->setErrorMessage('Unrecognised option');
+                $choice->setErrorMessage('Unrecognised option');
 
-                $response = $this->question->ask($this->input, $this->output, $question);
+                $response = $this->question->ask($this->input, $this->output, $choice);
             }
 
             switch($response) {

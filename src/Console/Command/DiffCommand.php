51d72970f0afb602f3ebf31567cf6e4d9c3c027d
Will Pillar <will.pillar@graze.com>
2015-05-12 12:17:15 +0100

    Removing non-diff work from Differ and put it in DiffCommand

--- src/Console/Command/DiffCommand.php
+++ src/Console/Command/DiffCommand.php
@@ -2,20 +2,24 @@
 
 namespace Graze\Morphism\Console\Command;
 
+use Doctrine\DBAL\Connection;
+use Graze\Morphism\Config;
 use Graze\Morphism\Diff\Differ;
 use Graze\Morphism\Diff\DifferConfiguration;
+use Graze\Morphism\Parse\Token;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class DiffCommand extends Command
 {
     protected function configure()
     {
         $this->setName('diff')
-            ->setDescription('Greet someone')
+            ->setDescription('Extracts schema definitions from the named connections, and outputs the necessary statements to transform them into what is defined under the schema path.')
             ->addArgument(
                 'config-file',
                 InputArgument::REQUIRED,
@@ -93,8 +97,134 @@ class DiffCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $config = DifferConfiguration::buildFromInput($input);
-        $differ = new Differ($config);
-        $differ->run();
+        $diffConfig = DifferConfiguration::buildFromInput($input);
+        $differ = new Differ($diffConfig);
+
+        $config = new Config($diffConfig->getConfigFile());
+        $config->parse();
+
+        $connectionNames =
+            (count($diffConfig->getConnectionNames()) > 0)
+                ? $diffConfig->getConnectionNames()
+                : $config->getConnectionNames();
+
+        Token::setQuoteNames($diffConfig->isQuoteNames());
+
+//        $logDir = $this->config->getLogDir();
+//
+//        if ($logDir !== null
+//            && ! is_dir($logDir)
+//            && ! @mkdir($logDir, 0777, true)) {
+//            fprintf(STDERR, "Could not create log directory: {$logDir}\n");
+//            exit(1);
+//        }
+
+        foreach ($connectionNames as $connectionName) {
+            $output->writeln('---------------------------------');
+            $output->writeLn('    <comment>Connection: ' . $connectionName . '</comment>');
+            $output->writeln('---------------------------------');
+            $output->writeln('');
+            $connection = $config->getConnection($connectionName);
+            $entry = $config->getEntry($connectionName);
+            $dbName = $entry['connection']['dbname'];
+            $matchTables = [
+                $dbName => $entry['morphism']['matchTables']
+            ];
+
+            $diff = $differ->diff($connection, $matchTables);
+
+            foreach ($diff as $query) {
+                $output->writeln('<info>' . $query . '</info>');
+                $output->writeln('');
+            }
+
+            $this->applyChanges($connection, $diffConfig, $output, $input, $diff);
+        }
+    }
+
+    protected function applyChanges(Connection $connection, DifferConfiguration $diffConfig, OutputInterface $output, InputInterface $input,  $diff)
+    {
+        if (count($diff) === 0) {
+            return;
+        }
+
+        if ($diffConfig->getApplyChanges() === 'no') {
+            return;
+        }
+
+        $confirm = $diffConfig->getApplyChanges() === 'confirm';
+        $logHandle = null;
+        $logDir = $diffConfig->getLogDir();
+
+//        if ($logDir !== null) {
+//            $logFile = "{$logDir}/{$connection->getDatabase()}.sql";
+//            $logHandle = fopen($logFile, 'w');
+//            if ($logHandle === false) {
+//                fprintf(STDERR, "Could not open log file for writing: $logFile\n");
+//                exit(1);
+//            }
+//        }
+
+        if (count($diff) > 0 && $confirm) {
+            $output->writeln('');
+            $output->writeln('<comment>-- Confirm changes to ' . $connection->getDatabase() . ':</comment>');
+        }
+
+        $defaultResponse = 'yes';
+
+        foreach($diff as $query) {
+            $response = $defaultResponse;
+            $apply = false;
+
+            if ($confirm) {
+                $output->writeln('');
+                $output->writeln('<info>' . $query . '</info>');
+                $output->writeln('');
+
+                $helper = $this->getHelper('question');
+                $question = new ChoiceQuestion(
+                    '-- Apply this change?',
+                    ['y' => 'yes', 'n' => 'no', 'a' => 'all', 'q' => 'quit']
+                );
+                $question->setErrorMessage('Unrecognised option');
+
+                $response = $helper->ask($input, $output, $question);
+            }
+
+            switch($response) {
+                case 'yes':
+                    $apply = true;
+                    break;
+
+                case 'no':
+                    $apply = false;
+                    break;
+
+                case 'all':
+                    $apply = true;
+                    $confirm = false;
+                    $defaultResponse = 'yes';
+                    break;
+
+                case 'quit':
+                    $apply = false;
+                    $confirm = false;
+                    $defaultResponse = 'no';
+                    break;
+            }
+
+            if ($apply) {
+//                if ($logHandle) {
+//                    fwrite($logHandle, "$query;\n\n");
+//                }
+                $connection->executeQuery($query);
+            } elseif ($logHandle && $diffConfig->isLogSkipped()) {
+//                fwrite($logHandle,
+//                    "-- [SKIPPED]\n" .
+//                    preg_replace('/^/xms', '-- ', $query) .  ";\n" .
+//                    "\n"
+//                );
+            }
+        }
     }
 }
--- src/Diff/Differ.php
+++ src/Diff/Differ.php
@@ -4,10 +4,8 @@ namespace Graze\Morphism\Diff;
 
 use Doctrine\DBAL\Connection;
 use Graze\Morphism\Parse\TokenStream;
-use Graze\Morphism\Parse\Token;
 use Graze\Morphism\Parse\MysqlDump;
 use Graze\Morphism\Extractor;
-use Graze\Morphism\Config;
 
 class Differ
 {
@@ -22,6 +20,32 @@ class Differ
     public function __construct(DifferConfiguration $config)
     {
         $this->config = $config;
+    }
+
+    /**
+     * @param Connection $connection
+     * @param array $matchTables
+     *
+     * @return array
+     */
+    public function diff(Connection $connection, $matchTables)
+    {
+        $currentSchema = $this->getCurrentSchema($connection);
+        $targetSchema = $this->getTargetSchema($connection);
+
+        $diff = $currentSchema->diff(
+            $targetSchema,
+            [
+                'createDatabase' => false,
+                'dropDatabase'   => false,
+                'createTable'    => $this->config->isCreateTable(),
+                'dropTable'      => $this->config->isDropTable(),
+                'alterEngine'    => $this->config->isAlterEngine(),
+                'matchTables'    => $matchTables
+            ]
+        );
+
+        return $diff;
     }
 
     /**
@@ -64,151 +88,5 @@ class Differ
             $this->config->getCollation(),
             $connection->getDatabase()
         );
-    }
-
-    /**
-     * @param Connection $connection
-     * @param array $diff
-     *
-     * @throws \Doctrine\DBAL\DBALException
-     * @throws \Exception
-     */
-    private function applyChanges(Connection $connection, $diff)
-    {
-        if (count($diff) === 0) {
-            return;
-        }
-
-        if ($this->config->getApplyChanges() === 'no') {
-            return;
-        }
-
-        $confirm = $this->config->getApplyChanges() === 'confirm';
-        $defaultResponse = 'y';
-        $logHandle = null;
-        $logDir = $this->config->getLogDir();
-
-        if ($logDir !== null) {
-            $logFile = "{$logDir}/{$connection->getDatabase()}.sql";
-            $logHandle = fopen($logFile, 'w');
-            if ($logHandle === false) {
-                fprintf(STDERR, "Could not open log file for writing: $logFile\n");
-                exit(1);
-            }
-        }
-
-        if (count($diff) > 0 && $confirm) {
-            echo "\n";
-            echo "-- Confirm changes to {$connection->getDatabase()}:\n";
-        }
-
-        foreach($diff as $query) {
-            $response = $defaultResponse;
-            $apply = false;
-
-            if ($confirm) {
-                echo "\n";
-                echo "$query;\n\n";
-                do {
-                    echo '-- Apply this change? [y]es [n]o [a]ll [q]uit: ';
-                    $response = fgets(STDIN);
-                    if ($response === false) {
-                        throw new \Exception('could not read response');
-                    }
-                    $response = rtrim($response);
-                }
-                while(!in_array($response, ['y', 'n', 'a', 'q']));
-            }
-
-            switch($response) {
-                case 'y':
-                    $apply = true;
-                    break;
-
-                case 'n':
-                    $apply = false;
-                    break;
-
-                case 'a':
-                    $apply = true;
-                    $confirm = false;
-                    $defaultResponse = 'y';
-                    break;
-
-                case 'q':
-                    $apply = false;
-                    $confirm = false;
-                    $defaultResponse = 'n';
-                    break;
-            }
-
-            if ($apply) {
-                if ($logHandle) {
-                    fwrite($logHandle, "$query;\n\n");
-                }
-                $connection->executeQuery($query);
-            } elseif ($logHandle && $this->config->isLogSkipped()) {
-                fwrite($logHandle,
-                    "-- [SKIPPED]\n" .
-                    preg_replace('/^/xms', '-- ', $query) .  ";\n" .
-                    "\n"
-                );
-            }
-        }
-    }
-
-    public function run()
-    {
-        $config = new Config($this->config->getConfigFile());
-        $config->parse();
-
-        $connectionNames =
-            (count($this->config->getConnectionNames()) > 0)
-                ? $this->config->getConnectionNames()
-                : $config->getConnectionNames();
-
-        Token::setQuoteNames($this->config->isQuoteNames());
-
-        $logDir = $this->config->getLogDir();
-
-        if ($logDir !== null
-            && ! is_dir($logDir)
-            && ! @mkdir($logDir, 0777, true)) {
-            fprintf(STDERR, "Could not create log directory: {$logDir}\n");
-            exit(1);
-        }
-
-        foreach ($connectionNames as $connectionName) {
-            echo "-- --------------------------------\n";
-            echo "--   Connection: $connectionName\n";
-            echo "-- --------------------------------\n";
-            $connection = $config->getConnection($connectionName);
-            $entry = $config->getEntry($connectionName);
-            $dbName = $entry['connection']['dbname'];
-            $matchTables = [
-                $dbName => $entry['morphism']['matchTables']
-            ];
-
-            $currentSchema = $this->getCurrentSchema($connection);
-            $targetSchema = $this->getTargetSchema($connection);
-
-            $diff = $currentSchema->diff(
-                $targetSchema,
-                [
-                    'createDatabase' => false,
-                    'dropDatabase'   => false,
-                    'createTable'    => $this->config->isCreateTable(),
-                    'dropTable'      => $this->config->isDropTable(),
-                    'alterEngine'    => $this->config->isAlterEngine(),
-                    'matchTables'    => $matchTables
-                ]
-            );
-
-            foreach ($diff as $query) {
-                echo "$query;\n\n";
-            }
-
-            $this->applyChanges($connection, $diff);
-        }
     }
 }

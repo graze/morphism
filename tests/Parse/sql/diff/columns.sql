29d405ccb01936bfb4a7f4e73d893d6f48c10bfd
Burhan Ali <burhan.ali@graze.com>
2017-09-28 16:28:09 +0100

    Add some more test cases

--- tests/Parse/sql/diff/columns.sql
+++ tests/Parse/sql/diff/columns.sql
@@ -25,3 +25,59 @@ create table t (a int);
 create table t (a char(10));
 ALTER TABLE `t`
 MODIFY COLUMN `a` char(10) DEFAULT NULL
+
+-- test - Make a column not null
+create table t (a int);
+create table t (a int not null);
+ALTER TABLE `t`
+MODIFY COLUMN `a` int(11) NOT NULL
+
+-- test - Make a column nullable
+create table t (a int not null);
+create table t (a int);
+ALTER TABLE `t`
+MODIFY COLUMN `a` int(11) DEFAULT NULL
+
+-- test - Redordering columns: Move a column to the start
+create table t (
+    a int,
+    b int,
+    c int
+);
+create table t (
+    c int,
+    a int,
+    b int
+);
+ALTER TABLE `t`
+MODIFY COLUMN `c` int(11) DEFAULT NULL FIRST
+
+-- test - Redordering columns: Move a column to the end
+create table t (
+    a int,
+    b int,
+    c int
+);
+create table t (
+    b int,
+    c int,
+    a int
+);
+ALTER TABLE `t`
+MODIFY COLUMN `b` int(11) DEFAULT NULL FIRST,
+MODIFY COLUMN `c` int(11) DEFAULT NULL AFTER `b`
+
+-- test - Redordering columns: Move a column in the middle
+create table t (
+    a int,
+    b int,
+    c int
+);
+create table t (
+    a int,
+    c int,
+    b int
+);
+ALTER TABLE `t`
+MODIFY COLUMN `c` int(11) DEFAULT NULL AFTER `a`
+
